15e295c41706cfdda143dd88628d9c6b85ea92a0
takaexe <164596411+takaexe@users.noreply.github.com>
2024-06-29 16:57:00 -0400

    support.js

--- commands/support.js
+++ commands/support.js
@@ -14,7 +14,7 @@ module.exports = {
             .setAuthor({
               name: 'Suporte bot',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://www.instagram.com/Taka.exe"
+              url: 'https://www.instagram.com/Taka.exe/"
           })
             .setDescription(`➡️ **Junte-se ao nosso servidor Discord para suporte e atualizações:**\n- ```diff
 Em Construção
