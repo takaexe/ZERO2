9d673a8e9feab8b249ded8a4ff17d8ba5d8cca1a
takaexe <164596411+takaexe@users.noreply.github.com>
2024-06-29 17:03:31 -0400

    support.js

--- commands/support.js
+++ commands/support.js
@@ -2,23 +2,24 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "support",
-  description: "Get support server link",
+  description: "Obtenha o link do servidor de suporte",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
     try {
 
-       const supportServerLink = "https://www.instagram.com/Taka.exe";
+      const supportServerLink = "https://discord.gg/";
+      const instagramLink = "https://www.instagram.com/Taka.exe";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Suporte bot',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
-              url: 'https://www.instagram.com/Taka.exe/"
+              url: 'https://www.instagram.com/Taka.exe/'
           })
             .setDescription(`➡️ **Junte-se ao nosso servidor Discord para suporte e atualizações:**\n- ```diff
 Em Construção
-```- ${supportServerLink}\n\n➡️ **Me siga:**\n- Instagram - ${supportServerLink}`)
+```- ${supportServerLink}\n\n➡️ **Me siga:**\n- Instagram - ${instagramLink}`)
             .setImage('https://media.discordapp.net/attachments/1067224490225959032/1256707652013129888/2560x1440-cyberpunk-1gsxtwvkfayit9el.png?ex=6681bfb4&is=66806e34&hm=e92fa220ead6d9f77153da7de0b0c9d5f9d789adc0c31d168b679801558a829f&=&format=webp&quality=lossless')
             .setTimestamp();
 
