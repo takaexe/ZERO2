ac93b220c6cac4fc8e91bd39962d6f6400c36eeb
takaexe <164596411+takaexe@users.noreply.github.com>
2024-06-29 16:30:41 -0400

    help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "help",
-  description: "Get information about the bot",
+  description: "Obtenha informações sobre o bot",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
@@ -12,10 +12,10 @@ module.exports = {
 
       const embed = new EmbedBuilder()
          .setColor('#0099ff')
-      .setTitle('💎 Prime Music Bot')
+      .setTitle('💎 Satoru Music')
       .setDescription('Bem-vindo ao Bot de Música!\n\n- Aqui estão os comandos disponíveis:\n\n' +
         '**/play :** Comece a tocar as músicas.\n' +
-        '**/ping :** erifique a latência do bot.\n' +
+        '**/ping :** Verifique a latência do bot.\n' +
         '**/support :** Exibir informações do servidor de suporte');
 
       return interaction.reply({ embeds: [embed] });
