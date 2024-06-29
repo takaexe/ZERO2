d224526b253918f1cfa9dee63568a0d4e2aacb2e
takaexe <164596411+takaexe@users.noreply.github.com>
2024-06-29 18:49:31 -0400

    help.js

--- commands/help.js
+++ commands/help.js
@@ -16,7 +16,7 @@ module.exports = {
       .setDescription('Bem-vindo ao Bot de Música!\n\n- Aqui estão os comandos disponíveis:\n\n' +
         '**/play :** Toque suas musicas pesquisando pelo nome ou link.\n' +
         '**/ping :** Verifique a latência do bot.\n' +
-        '**/suporte :**  ̶E̶x̶i̶b̶i̶r̶ ̶i̶n̶f̶o̶r̶m̶a̶ç̶õ̶e̶s̶ ̶d̶o̶ ̶s̶e̶r̶v̶i̶d̶o̶r̶ ̶d̶e̶ ̶s̶u̶p̶o̶r̶t̶e̶');
+        '**/suporte :** Informações de suporte do bot(contrução)');
 
       return interaction.reply({ embeds: [embed] });
     } catch (e) {
