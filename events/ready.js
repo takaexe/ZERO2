32a71f365b325c055c8ce4f331cda04a6217fa85
takaexe <164596411+takaexe@users.noreply.github.com>
2024-06-29 15:42:30 -0400

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -45,7 +45,7 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 const serverCount = client.guilds.cache.size;
 setInterval(() => client.user.setActivity({ 
-  name:`lavaLink GlaceYT`, 
+  name:`Manutenção!`, 
   type: ActivityType.Listening }), 10000);
 client.errorLog = config.errorLog
   
