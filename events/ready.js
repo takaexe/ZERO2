4e3e74974fd82544400aae84d5f6873c97b122c4
takaexe <164596411+takaexe@users.noreply.github.com>
2024-06-29 15:43:32 -0400

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,25 +1,3 @@
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
-
-
 const config = require("../config.js");
 const { ActivityType  } = require("discord.js")
 module.exports = async (client) => {
@@ -35,13 +13,13 @@ try {
 await rest.put(Routes.applicationCommands(client.user.id), {
 body: await client.commands,
 });
-console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded successfully!')
+console.log('\x1b[36m%s\x1b[0m', '|    🚀 Comandos carregados com sucesso!')
 } catch (err) {
-console.log('\x1b[36m%s\x1b[0m', '|    ❌ Commands Failed To Load!');
+console.log('\x1b[36m%s\x1b[0m', '|    ❌ Falha ao carregar comandos!');
 }
 })();
 
-console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
+console.log('\x1b[32m%s\x1b[0m', `|    🌼 logado como ${client.user.username}`);
 
 const serverCount = client.guilds.cache.size;
 setInterval(() => client.user.setActivity({ 
@@ -51,24 +29,4 @@ client.errorLog = config.errorLog
   
 }
 
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
 
