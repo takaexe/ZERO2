de00bb0976ae26960edf0eb685c62d617b50fe77
takaexe <164596411+takaexe@users.noreply.github.com>
2024-06-30 01:05:42 -0400

    player.js

--- player.js
+++ player.js
@@ -144,7 +144,7 @@ function initializePlayer(client) {
                 const pauseEmbed = new MessageEmbed()
                     .setColor("#FFA500")
                     .setTitle("Música pausada")
-                    .setAuthor('Música pausada', 'https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif')
+                    .setAuthor('Música pausada', 'https://cdn.discordapp.com/attachments/1156866389819281418/1157279084950176774/10173-stop.png')
                     .setDescription("A música está pausada!");
 
                 await channel.send({ embeds: [pauseEmbed] });
@@ -154,7 +154,7 @@ function initializePlayer(client) {
                 const playEmbed = new MessageEmbed()
                     .setColor("#00FF00")
                     .setTitle("Música reproduzida")
-                    .setAuthor('Música reproduzida', 'https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif')
+                    .setAuthor('Música reproduzida', 'https://cdn.discordapp.com/attachments/1156866389819281418/1157295079874016777/10449-music-note.png')
                     .setDescription("A música está sendo reproduzida!");
 
                 await channel.send({ embeds: [playEmbed] });
@@ -191,4 +191,18 @@ function initializePlayer(client) {
     }
 
     function clearQueue(player) {
-        player.queue
+        player.queue.clear();
+        queueNames.length = 0;
+    }
+
+    function showQueue(channel, queueNames) {
+        // Implementação para mostrar a fila de reprodução
+    }
+}
+
+module.exports = {
+    initializePlayer,
+    setLoop,
+    clearQueue,
+    showQueue
+};
