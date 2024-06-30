9d02d34ef6e739b78494e365a68f5de4aaa85e24
takaexe <164596411+takaexe@users.noreply.github.com>
2024-06-30 06:57:58 -0400

    avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,25 +1,32 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, User } = require('discord.js');
 
 module.exports = {
     name: 'avatar',
-    description: 'Mostra o avatar do usuário mencionado',
+    description: 'Mostra o avatar do usuário mencionado ou do autor da mensagem',
     execute(message, args) {
-        if (!message.mentions.users.size) {
-            const embed = new MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle(`${message.author.username}'s avatar`)
-                .setImage(message.author.displayAvatarURL({ format: 'png', dynamic: true }));
-            return message.channel.send({ embeds: [embed] });
+        let user;
+        
+        // Verifica se há menção a um usuário
+        if (message.mentions.users.size) {
+            user = message.mentions.users.first();
+        } else if (args.length) {
+            // Se não há menção, verifica se foi passado um ID ou nome
+            const searchString = args.join(' ');
+            user = message.client.users.cache.find(u => u.username === searchString || u.id === searchString);
+        } else {
+            // Caso contrário, mostra o avatar do autor da mensagem
+            user = message.author;
         }
 
-        const avatarList = message.mentions.users.map(user => {
-            const embed = new MessageEmbed()
-                .setColor('#0099ff')
-                .setTitle(`${user.username}'s avatar`)
-                .setImage(user.displayAvatarURL({ format: 'png', dynamic: true }));
-            return embed;
-        });
+        if (!user) {
+            return message.channel.send('Usuário não encontrado!');
+        }
 
-        message.channel.send({ embeds: avatarList });
+        const embed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${user.username}'s avatar`)
+            .setImage(user.displayAvatarURL({ format: 'png', dynamic: true }));
+
+        message.channel.send({ embeds: [embed] });
     },
 };
