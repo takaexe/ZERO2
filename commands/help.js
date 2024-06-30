41cb0488a7f291239557d445c39f7077e09ad047
takaexe <164596411+takaexe@users.noreply.github.com>
2024-06-29 22:32:47 -0400

    comandos.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js');
 
 
 module.exports = {
-  name: "help",
+  name: "comandos",
   description: "Obtenha informações sobre o bot",
   permissions: "0x0000000000000800",
   options: [],
@@ -14,9 +14,9 @@ module.exports = {
          .setColor('#0099ff')
       .setTitle('💎 Comandos!')
       .setDescription('Bem-vindo ao Bot de Música!\n\n- Aqui estão os comandos disponíveis:\n\n' +
-        '**/play :** Toque suas musicas pesquisando pelo nome ou link.\n' +
+        '**/play :** Adicione suas músicas pesquisando pelo nome ou link da sua playlist(Spotify, Youtube, Soundcloud)\n' +
         '**/ping :** Verifique a latência do bot.\n' +
-        '**/suporte :** Informações de suporte do bot(contrução)');
+        '**/suporte :** Informações de suporte do bot(EM CONSTRUÇÃO)');
 
       return interaction.reply({ embeds: [embed] });
     } catch (e) {
